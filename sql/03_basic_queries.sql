8424e7a08acd2d6fb434798bee332a20c6822a23
Palz <palomamavacala@hotmail.com>
2026-07-10 12:39:12 +0100

    Update 03_basic_queries.sql
    Create basic data exploration queries

--- sql/03_basic_queries.sql
+++ sql/03_basic_queries.sql
@@ -1,1 +1,47 @@
--- Contains basic SQL queries for analysing hospital appointments
+/*
+=========================================================
+Hospital Management SQL Portfolio Project
+File: 03_basic_queries.sql
+Author: Paloma Neto
+
+Objective:
+Answer basic business questions using SQL.
+=========================================================
+*/
+
+USE hospital_appointment_analysis;
+
+-- Question 1
+-- Display the first 20 patients
+
+SELECT *
+FROM patients
+LIMIT 20;
+
+-- Question 2
+-- Display the first 20 doctors
+
+SELECT *
+FROM doctors
+LIMIT 20;
+
+-- Question 3
+-- Display the first 20 appointments
+
+SELECT *
+FROM appointments
+LIMIT 20;
+
+-- Question 4
+-- Display the first 20 treatments
+
+SELECT *
+FROM treatments
+LIMIT 20;
+
+-- Question 5
+-- Display the first 20 billing records
+
+SELECT *
+FROM billing
+LIMIT 20;
